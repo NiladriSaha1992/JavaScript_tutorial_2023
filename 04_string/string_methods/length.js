dcc3e90447f0356316ac1a04b28d89f4f27c35ac
Niladri Saha <82999478+NiladriSaha1992@users.noreply.github.com>
2023-09-27 19:13:26 +0000

    string methods

---
+++ 04_string/string_methods/length.js
@@ -0,0 +1,9 @@
+const str = "Hello world, this is Niladri Saha, an aspiring full stack web developer!";
+console.log(`The above string has ${str.length} characters.`);
+
+const comment = "JavaScript is a great language of web.";
+if(comment.length > 60){
+  console.log("please write a comment below or equal to 60 characters");
+}else{
+  console.log(comment);
+}
