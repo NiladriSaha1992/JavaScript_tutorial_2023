fbccf353506c0065e5a82d13283ef7400a6cdb84
Niladri Saha <82999478+NiladriSaha1992@users.noreply.github.com>
2023-09-29 19:27:03 +0000

    Data types

---
+++ 02_datatypes/first/script.js
@@ -0,0 +1,22 @@
+// declaring variables with different data types
+
+// premitive data types
+const firstName = "Tista"; // String type data
+const lastName = "Dutta"; // String type data
+const age = 28; // Number type data
+const weight = 58.237; // float type data
+const isMarried = false; // Boolean type data
+let passportNumber; // Undefined type data (let is used here, because constants need to be initialized)
+const credits = null; // Null type data
+const favNumber = 28e320; // BigInt type data
+
+// let's check their data type (for this we need to use the "typeof" operator)
+console.log("Data type of firstName variable:", typeof firstName);
+console.log("Data type of lastName variable:", typeof lastName);
+console.log("Data type of age variable:", typeof age);
+console.log("Data type of weight variable:", typeof weight);
+console.log("Data type of isMarried variable:", typeof isMarried);
+console.log("Data type of passportNumber variable:", typeof passportNumber);
+console.log("Data type of credits variable:", typeof credits);
+console.log("Data type of favNumber variable:", typeof favNumber);
+
