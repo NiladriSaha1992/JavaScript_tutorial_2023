b03be655135b7798b9425d382a5610a35dc9a0e0
Niladri Saha <82999478+NiladriSaha1992@users.noreply.github.com>
2023-10-02 06:05:04 +0000

    Change in random password generator event handler

--- 14_DOM/01_basic_event_handlers/first/script.js
+++ 14_DOM/01_basic_event_handlers/first/script.js
@@ -1,6 +1,6 @@
 function generateRandomPassword(){
   const elem = document.getElementById("randPsw");
-  const str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$&";
   const pswArr = [];
   while(pswArr.length < 10){
     const randNum = Math.floor(Math.random() * str.length);
