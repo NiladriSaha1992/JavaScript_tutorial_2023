c79d736bfadabb37f04066737005ef45e813c3fb
Niladri Saha <82999478+NiladriSaha1992@users.noreply.github.com>
2023-10-02 06:01:53 +0000

    Basic event handler - random password generator

---
+++ 14_DOM/01_basic_event_handlers/first/script.js
@@ -0,0 +1,10 @@
+function generateRandomPassword(){
+  const elem = document.getElementById("randPsw");
+  const str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const pswArr = [];
+  while(pswArr.length < 10){
+    const randNum = Math.floor(Math.random() * str.length);
+    pswArr.push(str[randNum]);
+  }
+  elem.value = pswArr.join("");
+}
